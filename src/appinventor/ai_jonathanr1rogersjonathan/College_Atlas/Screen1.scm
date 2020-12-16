f647df0333b07bbf86ccc5e262ebc263670f4b57
Jonathan Rogers <jonathanr1rogersjonathan@outlook.com>
2020-12-16 12:34:26 -0500

    Add project files
    These are the initial project files for College Atlas. This is a RAW application and no changes were made other than the name of the project.

---
+++ src/appinventor/ai_jonathanr1rogersjonathan/College_Atlas/Screen1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":[],"YaVersion":"208","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"27","Uuid":"0","Title":"Screen1","AppName":"College_Atlas"}}
+|#
