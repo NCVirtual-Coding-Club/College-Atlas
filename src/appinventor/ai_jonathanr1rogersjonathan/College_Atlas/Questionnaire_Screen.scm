40e73ebadc2aea5f227ef83335ad9f1ebe940e10
Jonathan Rogers <jonathanr1rogersjonathan@outlook.com>
2021-01-18 17:41:41 -0500

    Remove unneeded files

--- src/appinventor/ai_jonathanr1rogersjonathan/College_Atlas/Questionnaire_Screen.scm
+++
@@ -1,4 +0,0 @@
-#|
-$JSON
-{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"208","Source":"Form","Properties":{"$Name":"Questionnaire_Screen","$Type":"Form","$Version":"27","ActionBar":"True","AppName":"College_Atlas","Theme":"AppTheme.Light.DarkActionBar","Title":"Questionnaire_Screen","TitleVisible":"False","Uuid":"0"}}
-|#
